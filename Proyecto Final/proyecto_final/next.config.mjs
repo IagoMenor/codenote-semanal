8a1dbc751b452fba52c09808bfe1b1e399f5a8d2
Iago Menor <menorvila@gmail.com>
2026-06-02 21:46:00 +0200

    correcciones despliegue 5

--- Proyecto Final/proyecto_final/next.config.mjs
+++ Proyecto Final/proyecto_final/next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["better-auth", "@better-auth/kysely-adapter", "kysely", "better-sqlite3"],
+  // Le damos a Turbopack el objeto vacío que nos pide para silenciar el error
+  experimental: {
+    turbopack: {}
+  },
   webpack: (config, { isServer }) => {
-    // Si estamos en el servidor, le decimos a Webpack que ignore por completo el compilador nativo de SQLite
     if (isServer) {
       config.externals.push("better-sqlite3");
     }
