446a4b3acbe75b3aa6564d472a703094bb3d68ad
Iago Menor <menorvila@gmail.com>
2026-06-05 22:35:42 +0200

    cambios auth-client

--- Proyecto_Final/proyecto_final/src/lib/auth-client.js
+++ Proyecto_Final/proyecto_final/src/lib/auth-client.js
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
-// Inicializamos el cliente apuntando a la URL de nuestro proyecto
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL 
+    baseURL: "https://codenote-semanal-vlqj.vercel.app"
 });
