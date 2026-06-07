1ab5ed4798f4dfb0b35d495cdc88170fd82b269d
IagoMenor <menorvila@gmail.com>
2026-06-07 10:24:21 +0200

    Delete Proyecto_Final/proyecto_final directory

--- Proyecto_Final/proyecto_final/src/app/api/auth/[...better-auth]/route.js
+++
@@ -1,5 +0,0 @@
-import { auth } from "@/lib/auth";
-
-// Exponemos los métodos GET y POST que usará el cliente de autenticación
-export const GET = auth.handler;
-export const POST = auth.handler;
--- Proyecto_Final/proyecto_final/src/lib/auth-client.js
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-    baseURL: "https://codenote-semanal-vlqj.vercel.app"
-});
